25d355e045e9dbebc58b76933b9815ec30eed653
Gabriel Cury <166022990+gabrielcury30@users.noreply.github.com>
2025-06-02 12:29:39 -0300

    Adicionar botao que redimensiona a grade

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 // Bloco que cria múltiplas divs na div pai de acordo com a quantidade de divs
-const quantidadeDivs = 256;
+let tamanhoGrade = 16;
+let quantidadeDivs = tamanhoGrade ** 2;
 
-const criarDivs = (quantidadeDivs) => {
+const criarDivs = (tamanhoGrade, quantidadeDivs) => {
     const divPai = document.querySelector(".conteiner");
 
-    const grade = Math.sqrt(quantidadeDivs);
-
-    const larguraDiv = `${100 / grade}%`;
-    const alturaDiv = `${100 / grade}%`;
+    let larguraDiv = `${100 / tamanhoGrade}%`;
+    let alturaDiv = `${100 / tamanhoGrade}%`;
 
     for (let i = 0; i < quantidadeDivs; i++) {
         const div = document.createElement("div");
@@ -21,12 +20,56 @@ const criarDivs = (quantidadeDivs) => {
     };
 };
 
-criarDivs(quantidadeDivs);
+criarDivs(tamanhoGrade, quantidadeDivs);
 
-const squareList = document.querySelectorAll(".square");
+// Criar evento que permite pintar os quadrados
+const pintarQuadrados = () => {
+    const squareList = document.querySelectorAll(".square");
 
-squareList.forEach(square => {
-    square.addEventListener("mouseover", () => {
-        square.style.backgroundColor = "red";
+    squareList.forEach(square => {
+        square.addEventListener("mouseover", () => {
+            square.style.backgroundColor = "red";
+        });
     });
+};
+
+pintarQuadrados();
+
+// Criação do botão e da lógica para mudar tamanho da grade
+const botaoMudarGrade = document.createElement("button");
+botaoMudarGrade.classList.add(".botaoMudarGrade");
+botaoMudarGrade.textContent = "Tamanho da Grade";
+
+const corpoDocumento = document.querySelector("body");
+corpoDocumento.prepend(botaoMudarGrade);
+
+botaoMudarGrade.addEventListener("click", () => {
+    tamanhoGrade = prompt("Digite o tamanho da grade NxN");
+
+    if (tamanhoGrade !== null) {
+        tamanhoGrade = tamanhoGrade.trim();
+    };
+
+    while (isNaN(tamanhoGrade) || tamanhoGrade === "") {
+        tamanhoGrade = prompt("Digite um tamanho de grade válida. Somente números.");
+
+        if (tamanhoGrade !== null) {
+            tamanhoGrade = tamanhoGrade.trim();
+        };
+    };
+
+    quantidadeDivs = tamanhoGrade ** 2;
+    
+    if (tamanhoGrade !== null) {
+        const divPai = document.querySelector(".conteiner");
+
+        const squareList = document.querySelectorAll(".square");
+
+        squareList.forEach(square => {
+            divPai.removeChild(square);
+        });
+
+        criarDivs(tamanhoGrade, quantidadeDivs);
+        pintarQuadrados();
+    };
 });
